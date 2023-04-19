a3a3b5055722906771bdc026e32c1d3c6f9be5d8
MarckJrquin <marck0208@outlook.com>
2023-04-19 15:17:29 -0500

    practica 2 / laboratorio 3 arreglo en try catch

--- Practica_2/app/src/main/java/com/example/applabs3_mnurinda_lyau_alinares/MainActivity.java
+++ Practica_2/app/src/main/java/com/example/applabs3_mnurinda_lyau_alinares/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,15 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNum1 = (EditText)findViewById(R.id.txtNum1);
-        txtNum2 = (EditText)findViewById(R.id.txtNum2);
-        tvRes = (TextView) findViewById(R.id.textView2);
-        btnCalcular = (Button) findViewById(R.id.btnCalcular);
-        spOperaciones = (Spinner) findViewById(R.id.spinner);
-
-        String [] ListaOperaciones = {"Sumar", "Restar", "Multiplicar", "Dividir"};
-        ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ListaOperaciones);
-        spOperaciones.setAdapter(Adapter);
+        this.InicialiarControles();
 
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,23 +33,41 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void calcular(){
-        int valor1 = Integer.parseInt(txtNum1.getText().toString());
-        int valor2 = Integer.parseInt(txtNum2.getText().toString());
-        String opcionSeleccionada = spOperaciones.getSelectedItem().toString();
+    private void InicialiarControles(){
+        txtNum1 = (EditText)findViewById(R.id.txtNum1);
+        txtNum2 = (EditText)findViewById(R.id.txtNum2);
+        tvRes = (TextView) findViewById(R.id.textView2);
+        btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        spOperaciones = (Spinner) findViewById(R.id.spinner);
 
-        if(opcionSeleccionada.equals("Sumar")){
-            int suma = valor1 + valor2;
-            tvRes.setText(String.valueOf(suma));
-        } else if (opcionSeleccionada.equals("Restar")) {
-            int resta = valor1 - valor2;
-            tvRes.setText(String.valueOf(resta));
-        } else if (opcionSeleccionada.equals("Multiplicar")) {
-            int multiplicacion = valor1 * valor2;
-            tvRes.setText(String.valueOf(multiplicacion));
-        } else if (opcionSeleccionada.equals("Dividir")) {
-            float division = (Float.parseFloat(String.valueOf(valor1)) / Float.parseFloat(String.valueOf(valor2)));
-            tvRes.setText(String.valueOf(division));
+        String [] ListaOperaciones = {"Sumar", "Restar", "Multiplicar", "Dividir"};
+        ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, ListaOperaciones);
+        spOperaciones.setAdapter(Adapter);
+    }
+
+    private void calcular(){
+
+        try {
+
+            int valor1 = Integer.parseInt(txtNum1.getText().toString());
+            int valor2 = Integer.parseInt(txtNum2.getText().toString());
+            String opcionSeleccionada = spOperaciones.getSelectedItem().toString();
+
+            if(opcionSeleccionada.equals("Sumar")){
+                int suma = valor1 + valor2;
+                tvRes.setText(String.valueOf(suma));
+            } else if (opcionSeleccionada.equals("Restar")) {
+                int resta = valor1 - valor2;
+                tvRes.setText(String.valueOf(resta));
+            } else if (opcionSeleccionada.equals("Multiplicar")) {
+                int multiplicacion = valor1 * valor2;
+                tvRes.setText(String.valueOf(multiplicacion));
+            } else if (opcionSeleccionada.equals("Dividir")) {
+                float division = (Float.parseFloat(String.valueOf(valor1)) / Float.parseFloat(String.valueOf(valor2)));
+                tvRes.setText(String.valueOf(division));
+            }
+        } catch (Exception e){
+            Toast.makeText(this, "Error en la operacion de los datos "+e.getMessage(), Toast.LENGTH_SHORT).show();
         }
     }
 }
