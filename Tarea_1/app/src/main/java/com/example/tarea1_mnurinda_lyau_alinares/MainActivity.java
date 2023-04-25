0fe4167793234fc03c6b8c0c9106a32bbacb9c4d
MarckJrquin <marck0208@outlook.com>
2023-04-24 23:14:05 -0500

    se creo y desarrollo el proyecto tarea_1

---
+++ Tarea_1/app/src/main/java/com/example/tarea1_mnurinda_lyau_alinares/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.tarea1_mnurinda_lyau_alinares;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
